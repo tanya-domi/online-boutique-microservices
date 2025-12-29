1a7060a84ff6d9a25e3dee12ee9ed9178e8517cb
Tanya Dominic Shey <sheytanyadominic@gmail.com>
2025-12-29 17:56:10 +0100

    updated Dockerfil with otel auto-instrumentation injection

--- cartservice/src/Program.cs
+++ cartservice/src/Program.cs
@@ -11,7 +11,6 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-// Added auto Otel Intrumentation injection to dockerfile
 
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
