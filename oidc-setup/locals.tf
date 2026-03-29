f22576db75409e362d96eb2991cc3d07d3713a4f
tanya-domi <sheytanyadominic@gmail.com>
2026-03-29 21:22:09 +0200

    [skip ci]

--- oidc-setup/locals.tf
+++
@@ -1,9 +0,0 @@
-locals {
-  gitlab_projects = [
-    "<gitlab-repo-username>/<Repo-Name>",
-    "<gitlab-repo-username>/<Repo-Name>"
-  ]
-  gitlab_project_conditions = [
-    for project in local.gitlab_projects : "project_path:${project}:ref_type:branch:ref:*"
-  ]
-}
--- oidc-setup/output.tf
+++
@@ -1,4 +0,0 @@
-output "gitlab_ci_role_arn" {
-  description = "The ARN of the IAM role for GitLab CI/CD."
-  value       = aws_iam_role.gitlab_ci_role.arn
-}
